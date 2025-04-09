fc1788037ca2ee7b2e66dfc7ff7bbb7a6760c4a5
Jennifer Tongco <jennifer.j.tongco@gmail.com>
2025-04-09 22:10:09 +0800

    modified route

--- website/views.py
+++ website/views.py
@@ -4,8 +4,9 @@ from .models import DryingRecord
 
 views = Blueprint('views', __name__)
 
-@views.route('/records')
+@views.route('/')
 @login_required
 def records():
     user_records = DryingRecord.query.filter_by(user_id=current_user.id).order_by(DryingRecord.timestamp.desc()).all()
     return render_template("records.html", user=current_user, records=user_records)
+
