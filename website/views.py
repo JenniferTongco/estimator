49554538fc51ebe26fa98b804b1d0f1c89f13755
Jennifer Tongco <jennifer.j.tongco@gmail.com>
2025-04-09 22:10:46 +0800

    redirect to the login page

--- website/views.py
+++ website/views.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 from .models import DryingRecord
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
-@login_required
-def records():
-    user_records = DryingRecord.query.filter_by(user_id=current_user.id).order_by(DryingRecord.timestamp.desc()).all()
-    return render_template("records.html", user=current_user, records=user_records)
-
+def home():
+    if current_user.is_authenticated:
+        user_records = DryingRecord.query.filter_by(user_id=current_user.id).order_by(DryingRecord.timestamp.desc()).all()
+        return render_template("records.html", user=current_user, records=user_records)
+    else:
+        return redirect(url_for('auth.login'))
